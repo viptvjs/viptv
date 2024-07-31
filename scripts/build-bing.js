c9545c5d8ef4825ed8abbef63114fa819bde3d4e
qq2236639958 <2236639958@qq.com>
2024-07-31 17:48:49 +0800

    feat: 试着提交
    不会写提交代码
    
    BREAKING CHANGE: 没有什么变更

--- scripts/build-bing.js
+++
@@ -1,8 +0,0 @@
-import spawn from 'cross-spawn';
-
-const proc = spawn('npm', ['run', 'build'], {
-    cwd: './packages/bing-wallpaper/',
-});
-
-proc.stdout.pipe(process.stdout);
-proc.stderr.pipe(process.stderr);
--- scripts/build-docs.js
+++
@@ -1,8 +0,0 @@
-import spawn from 'cross-spawn';
-
-const proc = spawn('npm', ['run', 'build'], {
-    cwd: './packages/artplayer-vitepress/',
-});
-
-proc.stdout.pipe(process.stdout);
-proc.stderr.pipe(process.stderr);
