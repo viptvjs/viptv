38a3ef2b2166921c74654fafdad2f8a71eb1a979
qq2236639958 <2236639958@qq.com>
2024-08-18 18:16:12 +0800

    修改了TYPES

--- packages/bing-wallpaper/src/index.ts
+++ packages/bing-wallpaper/src/index.ts
@@ -1,34 +1,4 @@
 import fetch from "node-fetch";
-type PictureInfo = {
-  hsh: string;
-  date: string;
-  title: string;
-  copyright: string;
-  url_preview: string;
-  url_1080: string;
-  url_4k: string;
-};
-
-type ArchivesInfo = {
-  date: string;
-  previewPath: string;
-  mapPath: string;
-};
-
-type JSONMap = {
-  images: PictureInfo[];
-  archives: ArchivesInfo[];
-};
-
-type SidebarItem = {
-  text: string;
-  link?: string;
-  icon?: string;
-  collapsible?: boolean;
-  collapsed?: boolean;
-  children?: SidebarItem[];
-  items?: SidebarItem[];
-};
 
 import {
   existsSync,
