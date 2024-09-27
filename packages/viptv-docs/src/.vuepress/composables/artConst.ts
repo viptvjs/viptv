343f4f3578dc1f5eb5fbd850a009922ee374d770
qq2236639958 <viptvx@gmail.com>
2024-09-27 09:17:40 +0800

    特性: 立日志文件

--- packages/viptv-docs/src/.vuepress/composables/artConst.ts
+++ packages/viptv-docs/src/.vuepress/composables/artConst.ts
@@ -1,6 +1,6 @@
 import { artplayerPlaylist } from "./artplayer-plugin-playlist.js";
-//import artplayerPluginDanmuku from "artplayer-plugin-danmuku";
-
+import artplayerPluginDanmuku from 'artplayer-plugin-danmuku';
+import artplayerProxyCanvas from 'artplayer-proxy-canvas'
 const poster = "https://img.viptv.work/iptv/ads.png";
 
 declare const DASHJS_INSTALLED: boolean;
@@ -96,27 +96,50 @@ export const mpConfig = (playlist: playlist[]) => {
     autoplay: true,
     autoOrientation: true,
     lock: true,
+    proxy: artplayerProxyCanvas(),
     setting: false,
     moreVideoAttr: {
       "webkit-playsinline": true,
       playsInline: true,
     },
     plugins: [
-      /* artplayerPluginDanmuku({
+      artplayerPluginDanmuku({
         danmuku: "https://d.artplayer.org/?id=BV1us421u7sp",
-        async beforeEmit(danmu) {
-          const isDirty = /fuck/i.test(danmu.text);
-          if (isDirty) return false;
-          const state = await saveDanmu(danmu);
-          return state;
+        speed: 5, // 弹幕持续时间，范围在[1 ~ 10]
+        margin: [10, '25%'], // 弹幕上下边距，支持像素数字和百分比
+        opacity: 1, // 弹幕透明度，范围在[0 ~ 1]
+        color: '#FFFFFF', // 默认弹幕颜色，可以被单独弹幕项覆盖
+        mode: 0, // 默认弹幕模式: 0: 滚动，1: 顶部，2: 底部
+        modes: [0, 1, 2], // 弹幕可见的模式
+        fontSize: 25, // 弹幕字体大小，支持像素数字和百分比
+        antiOverlap: true, // 弹幕是否防重叠
+        synchronousPlayback: false, // 是否同步播放速度
+        mount: undefined, // 弹幕发射器挂载点, 默认为播放器控制栏中部
+        heatmap: true, // 是否开启热力图
+        width: 512, // 当播放器宽度小于此值时，弹幕发射器置于播放器底部
+        points: [], // 热力图数据
+        filter: (danmu: { text: string | any[]; }) => danmu.text.length <= 100, // 弹幕载入前的过滤器
+        beforeVisible: () => true, // 弹幕显示前的过滤器，返回 true 则可以发送
+        visible: true, // 弹幕层是否可见
+        emitter: true, // 是否开启弹幕发射器
+        maxLength: 200, // 弹幕输入框最大长度, 范围在[1 ~ 1000]
+        lockTime: 5, // 输入框锁定时间，范围在[1 ~ 60]
+        theme: 'dark', // 弹幕主题，支持 dark 和 light，只在自定义挂载时生效
+        OPACITY: {}, // 不透明度配置项
+        FONT_SIZE: {}, // 弹幕字号配置项
+        MARGIN: {}, // 显示区域配置项
+        SPEED: {}, // 弹幕速度配置项
+        COLOR: [], // 颜色列表配置项
+        async beforeEmit(danmu: { text: string; }) {
+          return new Promise((resolve) => {
+            console.log(danmu);
+            setTimeout(() => {
+              resolve(true);
+            }, 1000);
+          });
+
         },
-        filter(danmu) {
-          return danmu.text.length <= 200;
-        },
-        async beforeVisible(danmu) {
-          return true;
-        },
-      }), */
+      }),
       artplayerPlaylist({
         rebuildPlayer: false, // 换P时重建播放器，默认false
         onchanged: () => { }, // 换P后的回调函数
