da0c46b121140dde018614cbdc0cba2e082bce5d
qq2236639958 <viptvx@gmail.com>
2024-10-02 04:38:55 +0800

    特性: git

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -21,50 +21,17 @@ module.exports = {
       confirmCommit: '是否提交或修改commit ?'
     },
     types: [
-      {
-        value: 'WIP',
-        name: '💪  WIP:      未完待续',
-      },
-      {
-        value: 'feat',
-        name: '✨  feat:     新的功能',
-      },
-      {
-        value: 'fix',
-        name: '🐞  fix:      Bug 修复',
-      },
-      {
-        value: 'refactor',
-        name: '🔨  refactor: 功能重构',
-      },
-      {
-        value: 'perf',
-        name: '🌸  perf:     性能优化',
-      },
-      {
-        value: 'docs',
-        name: '📚  docs:     文档相关',
-      },
-      {
-        value: 'test',
-        name: '🏁  test:     测试相关',
-      },
-      {
-        value: 'build',
-        name: '🧰  build:    构建或辅助工具的变动',
-      },
-      {
-        value: 'style',
-        name: '💅  style:    优化代码结构或格式',
-      },
-      {
-        value: 'revert',
-        name: '⏪  revert:   回退 commit',
-      },
-      {
-        value: 'chore',
-        name: '⚪  chore:    琐事，不属于以上任何类型',
-      },
+      { value: '特性', name: '特性:     新增功能' },
+      { value: '修复', name: '修复:     修复缺陷' },
+      { value: '文档', name: '文档:     文档变更' },
+      { value: '格式', name: '格式:     代码格式（不影响功能，例如空格、分号等格式修正）' },
+      { value: '重构', name: '重构:     代码重构（不包括 bug 修复、功能新增）' },
+      { value: '性能', name: '性能:     性能优化' },
+      { value: '测试', name: '测试:     添加疏漏测试或已有测试改动' },
+      { value: '构建', name: '构建:     构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）' },
+      { value: '集成', name: '集成:     修改 CI 配置、脚本' },
+      { value: '回退', name: '回退:     回滚 commit' },
+      { value: '其他', name: '其他:     对构建过程或辅助工具和库的更改（不影响源文件、测试用例）' },
     ],
     useEmoji: true,
     emojiAlign: 'center',
